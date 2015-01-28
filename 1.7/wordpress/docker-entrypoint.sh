4bb73e35e1bf13d26ed4d7fcc2c8181aaf3b1d59
Caleb Land <caleb@land.fm>
2015-01-27 19:24:28 -0500

    WIP

--- 1.7/wordpress/docker-entrypoint.sh
+++ 1.7/wordpress/docker-entrypoint.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -z "${ROOT}" ]; then
+    export ROOT=/srv
+fi
+
+if [ -z "${SERVER_NAME}" ]; then
+    export SERVER_NAME=localhost
+fi
+
+# Generate a canonical name if one isn't provided
+if [ -z "${CANONICAL_NAME}" ]; then
+    export CANONICAL_NAME="$(echo -n "${SERVER_NAME}" | cut -f 1 -d " ")"
+fi
+
 # create the environment variable include file to we can access our environment
 # from the nginx config
 env | while read e; do
@@ -8,5 +21,12 @@ env | while read e; do
     value="${e#$name=}"
     echo "set \$env_${name} ${value};" >> /etc/nginx/env.conf
 done
+
+# if the user has ssl keys, configure nginx with ssl
+if [ -f /etc/nginx/certs/ssl.key -a -f /etc/nginx/certs/ssl.crt ]; then
+    cp /etc/nginx/ssl.conf /etc/nginx/conf.d/ssl.conf
+else
+    cp /etc/nginx/default.conf /etc/nginx/conf.d/default.conf
+fi
 
 exec "$@"
---
+++ README.md
@@ -0,0 +1,11 @@
+# Docker Nginx
+
+## Environment Variables
+
+* `ROOT` - The directory where nginx will start serving (default /srv)
+* `SERVER_NAME` - Corrisponds to the server_name directive in nginx
+
+## Files
+
+* `/etc/nginx/ssl.crt`
+* `/etc/nginx/ssl.key`

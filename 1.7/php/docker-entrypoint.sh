5d8ef3c394d3dbf3d66aa84543725630b8d0b783
Caleb Land <caleb@land.fm>
2015-05-18 12:25:39 -0400

    Allow the user to specify the PHP_FPM filesize var
    This lets the user use one environment variable file to set the max
    filesize in both php-fpm and nginx-php

--- 1.7/php/docker-entrypoint.sh
+++ 1.7/php/docker-entrypoint.sh
@@ -8,7 +8,13 @@ export NGINX_PHP_CLIENT_MAX_BODY_SIZE
 require-link PHP_FPM php-fpm 9000 tcp
 
 if [ -z "${NGINX_PHP_CLIENT_MAX_BODY_SIZE}" ]; then
-  NGINX_PHP_CLIENT_MAX_BODY_SIZE="${PHP_FPM_ENV_PHP_FPM_UPLOAD_MAX_FILESIZE:-8m}"
+  if [ -n "${PHP_FPM_ENV_PHP_FPM_UPLOAD_MAX_FILESIZE}" ]; then
+    NGINX_PHP_CLIENT_MAX_BODY_SIZE="${PHP_FPM_ENV_PHP_FPM_UPLOAD_MAX_FILESIZE}"
+  elif [ -n "${PHP_FPM_UPLOAD_MAX_FILESIZE}" ]; then
+    NGINX_PHP_CLIENT_MAX_BODY_SIZE="${PHP_FPM_UPLOAD_MAX_FILESIZE}"
+  else
+    NGINX_PHP_CLIENT_MAX_BODY_SIZE="8m"
+  fi
 fi
 
 # Downcase the size (php uses uppercase postfixes and nginx uses lowercase)
